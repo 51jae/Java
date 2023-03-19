030bca38db97c01b8517a167ca9b4b2820376a54
51jae <staregreg01@naver.com>
2023-03-19 09:59:36 +0900

    report

---
+++ src/print/Print2.java
@@ -0,0 +1,14 @@
+package print;
+
+public class Print2
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("hello java");
+		//실행 "ctrl + f11
+		//주석	
+		System.out.println("java is simple");
+		System.out.print("안녕하세요");		//ln을 지우면 줄 바꿈을 안함
+		System.out.println("반갑습니다");
+	}
+}
---
+++ src/variable/Var2.java
@@ -0,0 +1,23 @@
+package variable;
+
+public class Var2 
+{
+	public static void main(String[] args)
+	{
+		
+		String var1 = "Jython";		//var1이라는 공간을 생성 java라는 데이터를 넣음
+		//java 가 변할 수 있기 떄문에 변수에다ㅣ가 저장
+		System.err.println("hello"+var1+"!");
+		System.out.println(var1+"is simple - > ");
+		System.out.println("안녕하세요 ");
+		System.out.println("반갑습니다 ");
+		System.out.println();
+		System.out.println(var1+"를 공부합니다 ");
+		System.out.println(var1);
+		
+		System.out.println(34-2);
+		//개발자가 32를 의도한건지 , 34-2 글자 그 자체를 의도한건지 컴은 알수가 없음
+		//사용자의 의도를 명확하게 나타내기 위해 변수에 '자료형'이라는 개념을 추가 
+		
+	}
+}
